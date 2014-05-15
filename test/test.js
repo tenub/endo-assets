089a37d555afc35f59f76fa555d493aa56d09040
jchrzan <jchrzan@webmd.net>
2014-05-15 16:40:22 -0400

    unformat test.js

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ describe('helper', function() {
 	describe('#parseValue', function() {
 		it('should return number of minutes for a properly formatted string', function() {
 			var h = new Helper();
-			assert.equal(1501,	h.parseValue('-1d 1h 1m',	'when'));
-			assert.equal(1500,	h.parseValue('-1d1h',		'when'));
-			assert.equal(1,		h.parseValue('-1m',			'when'));
-			assert.equal(false,	h.parseValue('-1m 1d',		'when'));
-			assert.equal(false,	h.parseValue('-60m',		'when'));
-			assert.equal(false,	h.parseValue('-24h',		'when'));
+			assert.equal(1501, h.parseValue('-1d 1h 1m', 'when'));
+			assert.equal(1500, h.parseValue('-1d1h', 'when'));
+			assert.equal(1, h.parseValue('-1m', 'when'));
+			assert.equal(false, h.parseValue('-1m 1d', 'when'));
+			assert.equal(false, h.parseValue('-60m', 'when'));
+			assert.equal(false, h.parseValue('-24h', 'when'));
 		});
 	});
 });
