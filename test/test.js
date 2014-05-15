0b6e61d260adb69783ff386798bc8b5f7915140d
jchrzan <jchrzan@webmd.net>
2014-05-15 16:38:48 -0400

    format test.js and cache test var

--- test/test.js
+++ test/test.js
@@ -3,12 +3,13 @@ var assert = require('assert'),
 describe('helper', function() {
 	describe('#parseValue', function() {
 		it('should return number of minutes for a properly formatted string', function() {
-			assert.equal(1501, (new Helper()).parseValue('-1d 1h 1m', 'when'));
-			assert.equal(1500, (new Helper()).parseValue('-1d1h', 'when'));
-			assert.equal(1, (new Helper()).parseValue('-1m', 'when'));
-			assert.equal(false, (new Helper()).parseValue('-1m 1d', 'when'));
-			assert.equal(false, (new Helper()).parseValue('-60m', 'when'));
-			assert.equal(false, (new Helper()).parseValue('-24h', 'when'));
+			var h = new Helper();
+			assert.equal(1501,	h.parseValue('-1d 1h 1m',	'when'));
+			assert.equal(1500,	h.parseValue('-1d1h',		'when'));
+			assert.equal(1,		h.parseValue('-1m',			'when'));
+			assert.equal(false,	h.parseValue('-1m 1d',		'when'));
+			assert.equal(false,	h.parseValue('-60m',		'when'));
+			assert.equal(false,	h.parseValue('-24h',		'when'));
 		});
 	});
 });
