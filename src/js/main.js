6806184a3819dabbbedeea80a6594b846f9accc8
jchrzan <jchrzan@webmd.net>
2016-01-04 17:22:32 -0500

    add manage user roles functionality

--- src/js/main.js
+++ src/js/main.js
@@ -48,4 +48,41 @@ $(function() {
 	$('.toggle-cb').on('click', function (e) {
 		$(this).parents('fieldset').find('input[type="checkbox"][name="' + this.id + '"]').prop('checked', this.checked);
 	});
+
+	$('.form-manage input, .form-manage select').on('change', function (e) {
+		$('.form-manage-save').prop('disabled', false);
+	});
+
+	$('.form-manage-save').on('click', function (e) {
+		$(this).prop('disabled', true);
+	});
+
+	$('.form-manage-role-add, .form-manage-role-remove').on('click', 'button', function (e) {
+		var $parent = $(this).parent();
+		var $user = $parent.parents('.manage-user');
+		var $selected = $parent.find('select option:selected');
+		var $userRoles = $user.find('.user-roles');
+
+		e.preventDefault();
+
+		if ($parent.hasClass('form-manage-role-add')) {
+			if (!$.grep($userRoles.find('li'), function (el) {
+				return $selected.val().length && $(el).data('roleId') == $selected.val();
+			}).length) {
+				$userRoles.append('<li data-role-id="' + $selected.val() + '">' + $selected.text() + '</li>');
+				$parent.find('option[value="' + $selected.val() + '"]').remove();
+				$user.find('.form-manage-role-remove select').append('<option value="' + $selected.val() + '">' + $selected.text() + '</option>');
+				$parent.find('select').trigger('change');
+			}
+		} else if ($parent.hasClass('form-manage-role-remove')) {
+			if ($.grep($userRoles.find('li'), function (el) {
+				return $selected.val().length && $(el).data('roleId') == $selected.val();
+			}).length) {
+				$userRoles.find('li[data-role-id="' + $selected.val() + '"]').remove();
+				$parent.find('option[value="' + $selected.val() + '"]').remove();
+				$user.find('.form-manage-role-add select').append('<option value="' + $selected.val() + '">' + $selected.text() + '</option>');
+				$parent.find('select').trigger('change');
+			}
+		}
+	});
 });
